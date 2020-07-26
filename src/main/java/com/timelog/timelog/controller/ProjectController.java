c5b7a09fac29b8a068dcba360d323c226ccd818c
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2020-07-26 11:47:01 -0400

    The Project Pagination has been updated

--- src/main/java/com/timelog/timelog/controller/ProjectController.java
+++ src/main/java/com/timelog/timelog/controller/ProjectController.java
@@ -9,14 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 import static com.timelog.timelog.constants.TimeLogConstants.*;
 
@@ -32,55 +31,114 @@ public class ProjectController {
         this.projectRepository = projectRepository;
     }
 
-    @GetMapping(PROJECTS_PATH)
-    public ResponseEntity<List<Project>> getProjectList(
-            @RequestParam(value = "projectList", required = false) Set<String> requestedProjectList,
-            @RequestParam(name = "page", required = false/*, defaultValue = "0"*/) Integer page,
-            @RequestParam(name = "size", required = false/*, defaultValue = "2"*/) Integer size) {
-        
-        List<Project> projectList;
-
-        if (requestedProjectList == null || requestedProjectList.isEmpty()) {
-            
-            projectList = getPagedProjectList(page, size);
-        } else {
-
-            projectList = getFilteredProjectList(requestedProjectList);
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+            return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
         }
-        
-        return new ResponseEntity<>(projectList, HttpStatus.OK); 
+
+        return Sort.Direction.ASC;
+    }
+//
+//    @GetMapping(PROJECTS_PATH)
+//    public ResponseEntity<List<Project>> getProjectList(
+//            @RequestParam(value = "projectList", required = false) Set<String> requestedProjectList,
+//            @RequestParam(name = "page", required = false/*, defaultValue = "0"*/) Integer page,
+//            @RequestParam(name = "size", required = false/*, defaultValue = "2"*/) Integer size) {
+//
+//        List<Project> projectList;
+//
+//        if (requestedProjectList == null || requestedProjectList.isEmpty()) {
+//
+//            projectList = getPagedProjectList(page, size);
+//        } else {
+//
+//            projectList = getFilteredProjectList(requestedProjectList);
+//        }
+//
+//        return new ResponseEntity<>(projectList, HttpStatus.OK);
+//    }
+
+    @GetMapping(PROJECTS_PATH)
+    public ResponseEntity<Map<String, Object>> getAllTutorialsPage(
+            @RequestParam(required = false) String name,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size,
+            @RequestParam(defaultValue = "id,asc") String[] sort) {
+
+        try {
+            List<Sort.Order> orders = new ArrayList<Sort.Order>();
+
+            if (sort[0].contains(",")) {
+                // will sort more than 2 fields
+                // sortOrder="field, direction"
+                for (String sortOrder : sort) {
+                    String[] _sort = sortOrder.split(",");
+                    orders.add(new Sort.Order(getSortDirection(_sort[1]), _sort[0]));
+                }
+            } else {
+                // sort=[field, direction]
+                orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
+            }
+
+            List<Project> projects = new ArrayList<Project>();
+            Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
+
+            Page<Project> pageTuts;
+            if (name == null)
+                pageTuts = projectRepository.findAll(pagingSort);
+            else
+                pageTuts = projectRepository.findByNameContaining(name, pagingSort);
+
+            projects = pageTuts.getContent();
+
+            if (projects.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("projects", projects);
+            response.put("currentPage", pageTuts.getNumber());
+            response.put("totalProjects", pageTuts.getTotalElements());
+            response.put("totalPages", pageTuts.getTotalPages());
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     
-    private List<Project> getFilteredProjectList(Set<String> requestedProjectList) {
-
-        List<Project> projectList;
-        Optional<List<Project>> optionalList = projectRepository.findByIdList(requestedProjectList);
-        if (!optionalList.isPresent()) {
-
-            throw new ProjectNotFoundException("");
-        }
-        projectList = optionalList.get();
-        return projectList;
-    }
-
-    private List<Project> getPagedProjectList(Integer page, Integer size) {
-
-        if (page == null ^ size == null) {
-            throw new ProjectPageParameterException();
-        }
-
-        List<Project> projectList;
-        if (page == null /*&& size == null*/)  {
-
-            projectList = projectRepository.findAll();
-        } else {
-
-            Pageable pageable = PageRequest.of(page, size);
-            Page<Project> requestedPage = projectRepository.findAll(pageable);
-            projectList = Lists.newArrayList(requestedPage);
-        }
-        return projectList;
-    }
+//    private List<Project> getFilteredProjectList(Set<String> requestedProjectList) {
+//
+//        List<Project> projectList;
+//        Optional<List<Project>> optionalList = projectRepository.findByIdList(requestedProjectList);
+//        if (!optionalList.isPresent()) {
+//
+//            throw new ProjectNotFoundException("");
+//        }
+//        projectList = optionalList.get();
+//        return projectList;
+//    }
+//
+//    private List<Project> getPagedProjectList(Integer page, Integer size) {
+//
+//        if (page == null ^ size == null) {
+//            throw new ProjectPageParameterException();
+//        }
+//
+//        List<Project> projectList;
+//        if (page == null /*&& size == null*/)  {
+//
+//            projectList = projectRepository.findAll();
+//        } else {
+//
+//            Pageable pageable = PageRequest.of(page, size);
+//            Page<Project> requestedPage = projectRepository.findAll(pageable);
+//            projectList = Lists.newArrayList(requestedPage);
+//        }
+//        return projectList;
+//    }
 
     @GetMapping(PROJECTS_PATH + "/{id}")
     public ResponseEntity<Project> getProjectById(@PathVariable("id") String id) {
--- src/main/java/com/timelog/timelog/repositories/ProjectRepository.java
+++ src/main/java/com/timelog/timelog/repositories/ProjectRepository.java
@@ -2,6 +2,8 @@ package com.timelog.timelog.repositories;
 
 
 import com.timelog.timelog.models.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -13,4 +15,6 @@ public interface ProjectRepository extends MongoRepository<Project, String> {
 
     @Query("{'_id': {'$in':?0}}")
     Optional<List<Project>> findByIdList(Set<String> projectList);
+
+    Page<Project> findByNameContaining(String name, Pageable pageable);
 }
